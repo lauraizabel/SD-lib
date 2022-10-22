1431a86263c6f81df13dbe5bb9e53011f7bc7329
Laura Izabel <lauraizabel8569@gmail.com>
2022-10-22 19:13:31 -0300

    dns service connection

--- src/lib/services/DnsConnectionService.ts
+++ src/lib/services/DnsConnectionService.ts
@@ -13,29 +13,39 @@ export interface DnsConfig {
 
 export default class DnsConnectionService {
   async setServerOn({ config, dnsAddress, dnsPort }: DnsConfig): Promise<any> {
-    try {
-      const { address, method, port, serviceName } = config;
+    const { address, method, port, serviceName } = config;
 
-      const configServer = {
-        address,
-        method, 
-        port, 
-        serviceName
-      };
+    const configServer = {
+      address,
+      method,
+      port,
+      serviceName,
+    };
 
-      await net
-        .connect(dnsPort, dnsAddress, () => {
-          console.info("Middleware connected DNS");
-        })
-        .on("data", (_) => {})
-        .on("error", (err) => console.log(err))
-        .write(JSON.stringify(configServer));
+    await net
+      .connect(dnsPort, dnsAddress, () => {
+        console.info("Middleware connected DNS");
+      })
+      .on("data", (_) => {})
+      .on("error", (err) => console.log(err))
+      .write(JSON.stringify(configServer));
 
-      return;
-    } catch (error) {
-      console.log(error);
-    }
+    return;
   }
 
-  async getIpAndPort() {}
+  async getIpAndPort(serviceName: string, dnsAddress: string, dnsPort: number) {
+    return new Promise((resolve, reject) => {
+      net
+        .connect(dnsPort, dnsAddress)
+        .on("data", (data) => {
+          try {
+            resolve(JSON.parse(data.toString()));
+          } catch (error) {
+            resolve(data.toString());
+          }
+        })
+        .on("error", (err) => reject(err))
+        .write(JSON.stringify({ serviceName }));
+    });
+  }
 }
